fa833f7885303e30446514b1cd3cf79a7cbe6509
erikadudki <edudkin@uni-bonn.de>
2021-05-27 16:18:42 +0200

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='czi2codex',
+    version='0.1.0',
+    author='Erika Dudkin',
+    author_email='erikadudkin@gmx.de',
+    description='Covert czi-files to CODEX format',
+    license='MIT License',
+    url='url = https://github.com/erikadudki/czi2codex.git',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+    ]
+)
