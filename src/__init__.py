78e0e63d251ecd774d176181d8b16eb653e1fcbf
erikadudki <edudkin@uni-bonn.de>
2021-06-16 14:47:57 +0200

    add init file

---
+++ src/__init__.py
@@ -0,0 +1,26 @@
+# # -*- coding: utf-8 -*-
+# __version__ = '0.1.0-dev'
+#
+# """
+# This file contains the initialization.
+# """
+# import os
+# import glob
+# import numpy as np
+# from czi2tif_codex import czi_to_tiffs
+# from generate_metadata_json import (meta_to_json, generate_std_options_file)
+# import argparse
+# import yaml
+# import xmltodict
+# from aicspylibczi import CziFile
+# import json
+# import lxml
+# from lxml import etree
+# from typing import Union
+# import shutil
+# from datetime import datetime
+# import tifffile
+# import warnings
+# from xml.etree import ElementTree
+# from itertools import product
+#
